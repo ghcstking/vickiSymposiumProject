58d0411e9ef4a9642a8a44e713793d2058ab6984
Student6 <Student6@10.8.33.116>
2017-05-09 12:30:11 -0400

    YEEEEEEE

--- vickiSymposiumProject/src/main/VickiSymposium.java
+++ vickiSymposiumProject/src/main/VickiSymposium.java
@@ -3,14 +3,22 @@ package main;
 import calendarFront.WelcomeScreen;
 import gui6.GUIApplication;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.table.*;
 
 public class VickiSymposium {
 	
-	private static VickiSymposium cal;
-	//private static WelcomeScreen mainScreen;
+	private static String name;
 
 	public VickiSymposium() {
 		
@@ -18,18 +26,27 @@ public class VickiSymposium {
 	
 	public static void main(String[] args) {
 		JFrame screen = new JFrame();
-		JTextField enter = new JTextField();
+		JTextField enter = new JTextField("Enter your name.");
+		JButton save = new JButton("SAVE");
 		
+		save.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				name = enter.getText();
+				System.out.println(name);
+			}
+			
+		});
+		screen.getContentPane().setLayout(null);
 		screen.setSize(1200, 1000);
-		enter.setBounds(0, 100, 10, 20);
+		//screen.getContentPane().setBackground(Color.BLACK);
+		save.setBounds(600, 535, 100, 30);
+		enter.setBounds(550, 500, 200, 30);
 		
+		screen.add(save);
 		screen.add(enter);
 		screen.setVisible(true);
-	}
-
-	public void initScreen() {
-		//mainScreen = new WelcomeScreen(getWidth(), getHeight());
-		//setScreen(mainScreen);
 	}
 
 }
