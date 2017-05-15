6376558388c3c15e21d02de31271c821edffce0c
Student6 <Student6@10.8.33.116>
2017-05-15 12:30:22 -0400

    sdfsdf

--- vickiSymposiumProject/src/main/VickiSymposium.java
+++ vickiSymposiumProject/src/main/VickiSymposium.java
@@ -45,8 +45,8 @@ public class VickiSymposium {
 	private static int thisDay;
 	private static GregorianCalendar greg;
 	private static String[] weekdays = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
-	private static String[] months = {"January", "February", "March", "April", "May", 
-			"June", "July", "August", "September", "October", "November", "December"};
+	private static String[] months = {"JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", 
+			"JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"};
 
 	public VickiSymposium() {
 		
@@ -85,53 +85,74 @@ public class VickiSymposium {
 		introScreen.setVisible(true);
 	}
 	
-	public static void switchToCalScreen() {
-		calendar = new JPanel();
+	public static void switchToCalScreen() {	
+		info();
+		instantiateEverything();
+		
 		calendar.setLayout(null);
 		calendar.setSize(1200, 1000);
-		
-		model = new DefaultTableModel(6, 7);
 		for (int i = 0; i < weekdays.length; i++) {
 			model.addColumn(weekdays[i]);
 		}
-//		for (int i = thisYear-10; i < thisYear+10; i++) {
-//			years.addItem(String.valueOf(i));
-//		}
-		cal = new JTable(model);
-		//years.setBounds(200, 200, 200, 200);
-		calendar.add(cal);
-		//calendar.add(years);
+		cal.setRowHeight(40);
+		model.setRowCount(6);
+		model.setColumnCount(7);
 		
-		mth = new JLabel("JANUARY");
-		yr = new JLabel("2017");
-		mth.setFont(new Font("Helvetica", Font.ITALIC, 20));
+		for (int i = thisYear-10; i <= thisYear+10; i++) {
+			years.addItem(String.valueOf(i));
+		}
+		years.setBounds(200, 200, 200, 30);
+		
+		mth.setFont(new Font("Helvetica", Font.BOLD, 40));
 		yr.setFont(new Font("Helvetica", Font.BOLD, 50));
-		mth.setBounds(300, 300, 100, 100);
+		mth.setBounds(600, 100, 100, 100);
 		yr.setBounds(590, 25, 200, 40);
-		
-		prev = new JLabel();
-		next = new JLabel();
 		prev.setIcon(new ImageIcon("resources/left.png"));
-		prev.setBounds(100, 25, 50, 50);
+		prev.setBounds(100, 120, 50, 50);
 		next.setIcon(new ImageIcon("resources/right.png"));
-		next.setBounds(1100, 25, 50, 50);
+		next.setBounds(1100, 120, 50, 50);
 		
+		calendar.add(cal);
+		calendar.add(years);
 		calendar.add(prev);
 		calendar.add(next);
 		calendar.add(yr);
-		//calendar.add(mth);
+		calendar.add(mth);
 		introScreen.add(calendar);
+		
+		drawCal(thisMonth, thisYear);
 		
 		save.setVisible(false);
 		enter.setVisible(false);	
 	}
 	
 	public static void info() {
+		greg = new GregorianCalendar();
 		thisMonth = greg.get(GregorianCalendar.MONTH);
 		thisYear = greg.get(GregorianCalendar.YEAR);
 		thisDay = greg.get(GregorianCalendar.DAY_OF_MONTH);
 	}
 	
 	public static void drawCal(int month, int yr) {
+		prev.setEnabled(true);
+		next.setEnabled(true);
+		mth.setText(months[month]);
+		if (month == 0 && yr <= thisYear-10) {
+			prev.setEnabled(false);
+		}
+		if (month == 11 && yr >= thisYear+10) {
+			next.setEnabled(false);
+		}
+	}
+	
+	public static void instantiateEverything() {
+		years = new JComboBox();
+		calendar = new JPanel();
+		cal = new JTable(model);
+		mth = new JLabel("JANUARY");
+		yr = new JLabel("2017");
+		model = new DefaultTableModel(6, 7);
+		prev = new JLabel();
+		next = new JLabel();
 	}
 }
