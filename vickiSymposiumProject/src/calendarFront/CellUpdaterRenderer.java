4ac8af2e77309c80ccba1f55db7e3353e531d0b2
ghcstking <vlau8050@bths.edu>
2017-06-14 21:10:50 -0400

    AAAAAAAAAA

--- vickiSymposiumProject/src/calendarFront/CellUpdaterRenderer.java
+++ vickiSymposiumProject/src/calendarFront/CellUpdaterRenderer.java
@@ -4,10 +4,13 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Toolkit;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
+
+import calendarBack.AnEvent;
 
 public class CellUpdaterRenderer extends DefaultTableCellRenderer {
 	private Component cellComponent;
@@ -15,10 +18,9 @@ public class CellUpdaterRenderer extends DefaultTableCellRenderer {
 	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean 
 			hasFocus, int row, int column) {
 		cellComponent = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-		
 		((JLabel) cellComponent).setHorizontalAlignment(JLabel.LEFT);
-		((JLabel) cellComponent).setVerticalAlignment(JLabel.BOTTOM);
-		
+		((JLabel) cellComponent).setVerticalAlignment(JLabel.TOP);
+		((JLabel) cellComponent).setIcon(new ImageIcon("resources/sun.png"));
 		return cellComponent;
 	}
 }
