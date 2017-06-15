b85bf40b9981b075d750b658bb889b91294c66e0
ghcstking <vlau8050@bths.edu>
2017-06-14 22:55:17 -0400

    Done

--- vickiSymposiumProject/src/calendarBack/CellRenderer.java
+++ vickiSymposiumProject/src/calendarBack/CellRenderer.java
@@ -5,6 +5,7 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -24,16 +25,8 @@ public class CellRenderer extends DefaultTableCellRenderer {
 		scroller = new JScrollPane(cellComponent, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		width = (int) screenSize.getWidth()/7;
-//		if (table.getMouseListeners().) {
-//			System.out.println("YE");
-//			JLabel newLabel = new JLabel();
-//			String original = table.getModel().getValueAt(row, column).toString();
-//			//newLabel.setText("<html><p>" + original + "AAA" + "</p></html>");
-//			((JLabel) cellComponent).setText("<html><p>" + original + "AAA" + "</p></html>");
-//		}
 		((JLabel) cellComponent).setHorizontalAlignment(JLabel.LEFT);
 		((JLabel) cellComponent).setVerticalAlignment(JLabel.TOP);
-//		((JLabel) cellComponent).setText("<html><p style=\"width:" + width+ "px\"></p></html>");
 		if (value != null && (column == 0 || column == 6)){
 			cellComponent.setBackground(new Color(173, 190, 219));
 		}
