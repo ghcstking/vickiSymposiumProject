33e083bd09eb2dad4135eb26f32682d9d4546197
ghcstking <vlau8050@bths.edu>
2017-06-14 22:54:06 -0400

    For the symposium, this never actually runs

--- vickiSymposiumProject/src/calendarFront/CellUpdaterRenderer.java
+++ vickiSymposiumProject/src/calendarFront/CellUpdaterRenderer.java
@@ -18,9 +18,13 @@ public class CellUpdaterRenderer extends DefaultTableCellRenderer {
 	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean 
 			hasFocus, int row, int column) {
 		cellComponent = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-		((JLabel) cellComponent).setHorizontalAlignment(JLabel.LEFT);
-		((JLabel) cellComponent).setVerticalAlignment(JLabel.TOP);
-		((JLabel) cellComponent).setIcon(new ImageIcon("resources/sun.png"));
-		return cellComponent;
+		if (isSelected && value != null) {
+			((JLabel) cellComponent).setHorizontalAlignment(JLabel.LEFT);
+			((JLabel) cellComponent).setVerticalAlignment(JLabel.TOP);
+			String original = value.toString();
+			((JLabel) cellComponent).setText(original + "\n" + AnEvent.eventName());
+			return cellComponent;
+		}
+		return null;
 	}
 }
